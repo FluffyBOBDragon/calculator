87e08ae9b99fc799376f78cc69336d9c8d405521
Jedd Miguel <jeddjoshua.miguel@warriorlife.net>
2017-12-13 11:13:38 -0800

    fixed more bugs

--- calculator.js
+++ calculator.js
@@ -4,20 +4,18 @@ function v(val) {
 	document.getElementById("d").value += val;
 }
 
-/*
+
 function d(val) {
 	document.getElementById("d").value=val;
 }
-function v(val) {
-	document.getElementById("d").value += val;
 
-}
 
 function e(){
 	try {
-		d(eval(document.getElementById("d".value));
+		d(eval(document.getElementById("d").value));
 	}
 	catch(err) {
 		d("Error");
 	}
-}*/
+}
+
